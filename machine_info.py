718856d65c03c19369cca1b03f1d2137501af51d
Marilzon <maxmaril@hotmail.com>
2022-01-06 16:27:50 -0300

    machine infos with platform package

---
+++ machine_info.py
@@ -0,0 +1,8 @@
+import platform
+
+print('Nome da maquina: ', platform.node())
+print('Arquiquetura: ', platform.architecture())
+print('S.O: ', platform.system())
+print('Versao: ', platform.release())
+print('Processador: ', platform.processor())
+print('Versao do python: ', platform.processor())
