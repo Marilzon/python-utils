966721585c4428db990fb01616f88a70a000aade
Marilzon <maxmaril@hotmail.com>
2022-01-06 16:45:00 -0300

    getpass lib to return user in computer

--- machine_info.py
+++ machine_info.py
@@ -1,8 +1,17 @@
 import platform
+from datetime import datetime
+import getpass
 
+print("Usuario: ", getpass.getuser())
 print('Nome da maquina: ', platform.node())
 print('Arquiquetura: ', platform.architecture())
 print('S.O: ', platform.system())
 print('Versao: ', platform.release())
 print('Processador: ', platform.processor())
 print('Versao do python: ', platform.processor())
+
+print(
+  'Data: ',
+  datetime.now().day,"/",
+  datetime.now().month,"/",
+  datetime.now().year)
