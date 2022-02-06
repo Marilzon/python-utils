2e86e61f7bc8e9602e384ab1521e166396c58752
Marilzon <maxmaril@hotmail.com>
2022-02-06 16:43:36 -0300

    UPDATE: table_calc code

--- table_calc.py
+++ table_calc.py
@@ -1,6 +1,34 @@
 print("\n******************* Python Calculator *******************")
 
 
-def calculator(number):
-  for index in range(1,11):
-    print(number, "X", index, "=",number * index)
+print("Selecione a  operacao desejada\n")
+
+print("1 - Soma")
+print("2 - Subtracao")
+print("3 - Multiplicacao")
+print("4 - Divisao")
+
+operation = int(input())
+
+if (operation == 1):
+  number_one = int(input("Digite o primeiro valor: "))
+  number_two = int(input("Digite o segundo valor: "))
+  print("Resultado: ", number_one + number_two)
+
+if (operation == 2):
+  number_one = int(input("Digite o primeiro valor: "))
+  number_two = int(input("Digite o segundo valor: "))
+  print("Resultado: ", number_one - number_two)
+
+if (operation == 3):
+  number_one = int(input("Digite o primeiro valor: "))
+  number_two = int(input("Digite o segundo valor: "))
+  print("Resultado: ", number_one * number_two)
+
+if (operation == 4):
+  number_one = int(input("Digite o primeiro valor: "))
+  number_two = int(input("Digite o segundo valor: "))
+  print("Resultado: ", number_one / number_two)
+
+if (operation >= 5):
+  print("Operacao invalida")
