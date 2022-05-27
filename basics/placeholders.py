0beceba4aacb37f65c16eb6dd1d4276cbf7b2ed5
marilzon <maxmaril@hotmail.com>
2022-05-27 19:13:58 -0300

    refact placeholders.py

--- basics/placeholders.py
+++ basics/placeholders.py
@@ -1,8 +1,6 @@
 a = 1;b = 2;c = 3;d = 4;e = 5
 
-data = "a: {},b: {},c: {},d: {},e: {}"
-
-print(data.format(a, b, c, d, e))
+print(f"a: {a},b: {b},c: {c},d: {d},e: {e}")
 
 # Placeholders attributes is a implicit string type to print items
 # \' \" \n \r \t \b"
