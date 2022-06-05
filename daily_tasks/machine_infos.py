c3f40b34f177279186b7c0ce5489070b88f2e927
marilzon <maxmaril@hotmail.com>
2022-06-05 13:52:13 -0300

    refact machines_info.py to functional algoritmh

--- daily_tasks/machine_infos.py
+++ daily_tasks/machine_infos.py
@@ -3,16 +3,18 @@ from datetime import date
 import getpass
 import socket
 
-print("Usuario: ", getpass.getuser())
-print('Nome da maquina: ', platform.node())
-print('Arquiquetura: ', platform.architecture())
-print('S.O: ', platform.system())
-print('Versao: ', platform.release())
-print('Processador: ', platform.processor())
-print('Versao do python: ', platform.python_version())
-print('IP Local: ', socket.gethostbyname(socket.gethostname()))
+def machine_infos():
+    print("Usuario: ", getpass.getuser())
+    print('Nome da maquina: ', platform.node())
+    print('Arquiquetura: ', platform.architecture())
+    print('S.O: ', platform.system())
+    print('Versao: ', platform.release())
+    print('Processador: ', platform.processor())
+    print('Versao do python: ', platform.python_version())
+    print('IP Local: ', socket.gethostbyname(socket.gethostname()))
+    # CONVERTING DATE STYLE TO BRAZILLIAN DEFAULT
+    actual_date = date.today()
+    print('Data da consulta: ', actual_date.strftime('%d/%m/%Y'))
+    return
 
-# CONVERTING DATE STYLE TO BRAZILLIAN DEFAULT
-
-actual_date = date.today()
-print('Data da consulta: ', actual_date.strftime('%d/%m/%Y'))
+machine_infos()
