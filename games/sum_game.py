65d3ce041748b13a565a8fc9c41d75e5a049ead0
marilzon <maxmaril@hotmail.com>
2022-06-05 13:37:00 -0300

    refactoring sum_game to functional algoritmh

--- games/sum_game.py
+++ games/sum_game.py
@@ -3,19 +3,24 @@ import os
 
 os.system("clear")
 
-error_count = 0
-x = random.randrange(0, 100)
-y = random.randrange(0, 100)
-print("Answer the sum: ", x, "+", y, " = ", "?")
-result = x + y
-player_result = int(input("Input the result: "))
+def sum_game():
+    error_count = 0
+    x = random.randrange(0, 100)
+    y = random.randrange(0, 100)
+    print("Answer the sum: ", x, "+", y, " = ", "?")
+    result = x + y
+    player_result = int(input("Input the result: "))
 
-while player_result != result:
-    error_count += 1
-    if (result > player_result):
-        print("Wrong!, this value is bigger, errors: ", error_count)
-    elif (result < player_result):
-        print("Wrong!, this value is smaller, ", error_count)
-    player_result = int(input("Try new response: "))
-print("---you win---, the result is: ",  str(player_result))
-print("you used", str(error_count + 1), "attempts")
+    while player_result != result:
+        error_count += 1
+        if (result > player_result):
+            print("Wrong!, this value is bigger, errors: ", error_count)
+        elif (result < player_result):
+            print("Wrong!, this value is smaller, ", error_count)
+        player_result = int(input("Try new response: "))
+    print("---you win---, the result is: ",  str(player_result))
+    print("you used", str(error_count + 1), "attempts")
+
+    return
+
+sum_game()
